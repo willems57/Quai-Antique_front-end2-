e6361b6728125c0849c7346d731586aebcd197b8
willems57 <jeanwillems@hotmail.fr>
2024-11-15 11:49:04 +0100

    mise en place des page inscription et connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,9 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-    new Route("/galerie", "La galerie", "/pages/galerie.html", [], "/js/auth/galerie.js"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", [], "/js/auth/signup.js"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
     new Route("/account", "Mon compte", "/pages/account.html", ["client", "admin"]),
     new Route("/editpassword", "changement de mot de passe", "/pages/auth/editpassword.html", ["client", "admin"]),
     new Route("/allResa", "Vos reservations", "/pages/reservation/allResa.html", ["client"]),
